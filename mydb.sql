e19ae7332f849dafdeeb6993d4e51e6c0e365936
Raheen7861 <raheenmultani786@gmail.com>
2022-09-02 11:13:56 +0530

    Fourth Commit

---
+++ mydb.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE resumebuilder;
+CREATE TABLE register (user VARCHAR(20),email VARCHAR(30),pass VARCHAR(20),cpass VARCHAR(20));
+INSERT INTO register (user,email,pass,cpass) VALUES ("admin","admin@gmail.com","admin","admin");
